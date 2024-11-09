c16ce490d26d22dbd9feca519587a6272fccc952
Blackgodd <williamblackomre@gmail.com>
2024-11-08 16:00:38 -0800

    all

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,8 +2,8 @@
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
-import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Footer from './components/Footer'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -38,8 +38,8 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile  name ="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
-      <UserProfile name ="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
