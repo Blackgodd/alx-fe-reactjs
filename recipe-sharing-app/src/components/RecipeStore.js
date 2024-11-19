e00eb3a3b7ac9ac54ae2566b1a76f7c9a860463d
Blackgodd <williamblackomre@gmail.com>
2024-11-18 16:24:39 -0800

    all

--- recipe-sharing-app/src/components/RecipeStore.js
+++ recipe-sharing-app/src/components/RecipeStore.js
@@ -1,15 +1,10 @@
-import create from 'zustand';
+import create from 'zustand'
 
-const useRecipeStore = create((set) => ({
+const useRecipeStore = create(set => ({
   recipes: [],
-  initializeRecipes: (initialRecipes) => {
-    set({ recipes: initialRecipes });
-  },
-  addRecipe: (recipe) => {
-    set((state) => ({
-      recipes: [...state.recipes, recipe],
-    }));
-  },
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
 }));
 
-export default useRecipeStore;
+
+export default useRecipeStore
