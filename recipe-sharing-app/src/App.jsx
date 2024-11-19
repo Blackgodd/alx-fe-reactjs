0de4542a497e088a4c804f838a2b64334b01f044
Blackgodd <williamblackomre@gmail.com>
2024-11-18 23:01:48 -0800

    all

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import EditRecipeForm from './components/EditRecipeForm';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Manager</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Manager</h1>
+        <Routes>
+          {/* Home Page - Displays the list of recipes */}
+          <Route path="/" element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          } />
+
+          {/* Recipe Details Page */}
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+
+          {/* Edit Recipe Page */}
+          <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
